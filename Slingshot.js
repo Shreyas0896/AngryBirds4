2a29e664b5173afd6b5a62e5302eb4b56ae144de
Shreyas0896 <shreyas.kotla@gmail.com>
2020-04-30 21:48:12 -0500

    / we created the catapault and the rubber band

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,12 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+
+
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +22,35 @@ class SlingShot{
     }
 
     display(){
+        
+        image(this.sling1,140,25)
+        image(this.sling2,115,25)
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
-            var pointB = this.pointB;
+            var pointB = this.pointB; 
+            push();
+
+            stroke(255,255,255)
+
+        if(pointA.x<220){
+
             strokeWeight(4);
+           
             line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x,pointA.y,pointB.x+20,pointB.y-3);
+            image(this.sling3,pointA.x-10,pointA.y-10,15,30)
+
+        }
+else {
+    strokeWeight(4);
+           
+    line(pointA.x, pointA.y, pointB.x, pointB.y);
+    line(pointA.x,pointA.y,pointB.x+20,pointB.y-3);
+    image(this.sling3,pointA.x+15,pointA.y-10,15,30)
+}
+            
+            pop();
         }
     }
     
